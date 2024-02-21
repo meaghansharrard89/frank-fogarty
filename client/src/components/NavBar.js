a00dac42bd877503228d94338a439463a91460fc
Meaghan <meaghan.sharrard89@gmail.com>
2024-02-20 18:30:24 -0800

    update home page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,69 +1,93 @@
 import React, { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 function NavBar() {
+  const location = useLocation(); // Get current location
+  const [currentPage, setCurrentPage] = useState(location.pathname); // Set initial current page
+
   return (
     <>
-      <nav class="bg-gray-800">
-        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div class="relative flex h-16 items-center justify-between">
-            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+      <nav className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               <button
                 type="button"
-                class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               ></button>
             </div>
-            <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div class="flex flex-shrink-0 items-center"></div>
-              <div class="hidden sm:ml-6 sm:block">
-                <div class="flex space-x-4">
-                  <a
-                    href="/"
-                    class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                    aria-current="page"
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-shrink-0 items-center"></div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  <NavLink
+                    exact
+                    to="/"
+                    className={`${
+                      currentPage === "/"
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                    } rounded-md px-3 py-2 text-sm font-medium`}
+                    onClick={() => setCurrentPage("/")}
                   >
                     HOME
-                  </a>
-                  <a
-                    href="/publication"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  </NavLink>
+                  <NavLink
+                    to="/publication"
+                    className={`${
+                      currentPage === "/publication"
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                    } rounded-md px-3 py-2 text-sm font-medium`}
+                    onClick={() => setCurrentPage("/publication")}
                   >
                     PUBLICATIONS
-                  </a>
-                  <a
-                    href="/team"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  </NavLink>
+                  <NavLink
+                    to="/team"
+                    className={`${
+                      currentPage === "/team"
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                    } rounded-md px-3 py-2 text-sm font-medium`}
+                    onClick={() => setCurrentPage("/team")}
                   >
                     TEAM
-                  </a>
-                  <a
-                    href="/research"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  </NavLink>
+                  <NavLink
+                    to="/research"
+                    className={`${
+                      currentPage === "/research"
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                    } rounded-md px-3 py-2 text-sm font-medium`}
+                    onClick={() => setCurrentPage("/research")}
                   >
                     RESEARCH
-                  </a>
+                  </NavLink>
                 </div>
               </div>
             </div>
-            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <button
                 type="button"
-                class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               >
-                <span class="absolute -inset-1.5"></span>
+                <span className="absolute -inset-1.5"></span>
               </button>
 
-              <div class="relative ml-3">
+              <div className="relative ml-3">
                 <div>
                   <button
                     type="button"
-                    class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                     id="user-menu-button"
                     aria-expanded="false"
                     aria-haspopup="true"
                   >
-                    <span class="absolute -inset-1.5"></span>
+                    <span className="absolute -inset-1.5"></span>
                   </button>
                 </div>
               </div>
@@ -71,33 +95,56 @@ function NavBar() {
           </div>
         </div>
 
-        <div class="sm:hidden" id="mobile-menu">
-          <div class="space-y-1 px-2 pb-3 pt-2">
-            <a
-              href="/"
-              class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-              aria-current="page"
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            <NavLink
+              exact
+              to="/"
+              className={`${
+                currentPage === "/"
+                  ? "bg-gray-900 text-white"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white"
+              } block rounded-md px-3 py-2 text-base font-medium`}
+              onClick={() => setCurrentPage("/")}
             >
               HOME
-            </a>
-            <a
-              href="/publication"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            </NavLink>
+            <NavLink
+              exact
+              to="/publication"
+              className={`${
+                currentPage === "/publication"
+                  ? "bg-gray-900 text-white"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white"
+              } block rounded-md px-3 py-2 text-base font-medium`}
+              onClick={() => setCurrentPage("/publication")}
             >
               PUBLICATIONS
-            </a>
-            <a
-              href="/team"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            </NavLink>
+            <NavLink
+              exact
+              to="/team"
+              className={`${
+                currentPage === "/team"
+                  ? "bg-gray-900 text-white"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white"
+              } block rounded-md px-3 py-2 text-base font-medium`}
+              onClick={() => setCurrentPage("/team")}
             >
               TEAM
-            </a>
-            <a
-              href="/research"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            </NavLink>
+            <NavLink
+              exact
+              to="/research"
+              className={`${
+                currentPage === "/research"
+                  ? "bg-gray-900 text-white"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white"
+              } block rounded-md px-3 py-2 text-base font-medium`}
+              onClick={() => setCurrentPage("/research")}
             >
               RESEARCH
-            </a>
+            </NavLink>
           </div>
         </div>
       </nav>
--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,106 +1,69 @@
 function Team() {
   return (
     <>
-      <section class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
-        <div class="text-center pb-12">
-          <h2 class="text-base font-bold text-indigo-600">
+      <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+        <div className="text-center pb-12">
+          <h2 className="text-base font-bold text-indigo-600">
             We have the best equipment in the market
           </h2>
-          <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+          <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
             Check our awesome team members
           </h1>
         </div>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div class="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                class="rounded-lg object-center object-cover h-auto w-full"
-                src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-                alt="photo"
-              />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-1 gap-6">
+          {teamMembers.map((member, index) => (
+            <div
+              key={index}
+              className="w-full bg-white rounded-lg shadow-lg overflow-hidden flex flex-col sm:flex-row items-center"
+            >
+              <div>
+                <img
+                  className="rounded-lg object-center object-cover w-full h-auto md:w-96 md:h-64"
+                  src={member.image}
+                  alt={member.name}
+                />
+              </div>
+              <div className="text-center py-8 sm:py-6 px-6">
+                <p className="text-xl text-gray-700 font-bold mb-2">
+                  {member.name}
+                </p>
+                <p className="text-base text-gray-400 font-normal">
+                  {member.job}
+                </p>
+                <p className="text-base text-gray-400 font-normal">
+                  {member.bio}
+                </p>
+              </div>
             </div>
-            <div class="text-center py-8 sm:py-6">
-              <p class="text-xl text-gray-700 font-bold mb-2">Dany Bailey</p>
-              <p class="text-base text-gray-400 font-normal">
-                Software Engineer
-              </p>
-            </div>
-          </div>
-          <div class="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                class="rounded-lg object-center object-cover h-auto w-full"
-                src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-                alt="photo"
-              />
-            </div>
-            <div class="text-center py-8 sm:py-6">
-              <p class="text-xl text-gray-700 font-bold mb-2">Lucy Carter</p>
-              <p class="text-base text-gray-400 font-normal">
-                Graphic Designer
-              </p>
-            </div>
-          </div>
-          <div class="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                class="rounded-lg object-center object-cover h-auto w-full"
-                src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80"
-                alt="photo"
-              />
-            </div>
-            <div class="text-center py-8 sm:py-6">
-              <p class="text-xl text-gray-700 font-bold mb-2">Jade Bradley</p>
-              <p class="text-base text-gray-400 font-normal">Dev Ops</p>
-            </div>
-          </div>
-          <div class="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                class="rounded-lg object-center object-cover h-auto w-full"
-                src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-                alt="photo"
-              />
-            </div>
-            <div class="text-center py-8 sm:py-6">
-              <p class="text-xl text-gray-700 font-bold mb-2">Dany Bailey</p>
-              <p class="text-base text-gray-400 font-normal">
-                Software Engineer
-              </p>
-            </div>
-          </div>
-          <div class="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                class="rounded-lg object-center object-cover h-auto w-full"
-                src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
-                alt="photo"
-              />
-            </div>
-            <div class="text-center py-8 sm:py-6">
-              <p class="text-xl text-gray-700 font-bold mb-2">Lucy Carter</p>
-              <p class="text-base text-gray-400 font-normal">
-                Graphic Designer
-              </p>
-            </div>
-          </div>
-          <div class="w-full bg-white rounded-lg sahdow-lg overflow-hidden flex flex-col justify-center items-center">
-            <div>
-              <img
-                class="rounded-lg object-center object-cover h-auto w-full"
-                src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80"
-                alt="photo"
-              />
-            </div>
-            <div class="text-center py-8 sm:py-6">
-              <p class="text-xl text-gray-700 font-bold mb-2">Jade Bradley</p>
-              <p class="text-base text-gray-400 font-normal">Dev Ops</p>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     </>
   );
 }
+
+const teamMembers = [
+  {
+    name: "Dany Bailey",
+    job: "Software Engineer",
+    bio: "Jessica Smith is a passionate software engineer with over a decade of experience in the tech industry. With a Bachelor's degree in Computer Science from Stanford University, she embarked on her journey to revolutionize the digital world. Jessica specializes in full-stack web development, leveraging her expertise in languages such as JavaScript, Python, and Java.",
+    image:
+      "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Lucy Carter",
+    job: "Graphic Designer",
+    bio: "Lucy Carter is a creative graphic designer with a passion for crafting visually stunning designs. With a Bachelor's degree in Graphic Design from Parsons School of Design, she brings a unique perspective to every project she tackles. Lucy's expertise lies in branding, typography, and user interface design.",
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Jade Bradley",
+    job: "Dev Ops",
+    bio: "Jade Bradley is a seasoned DevOps engineer with a knack for streamlining development processes and ensuring smooth deployments. With extensive experience in cloud computing platforms such as AWS and Azure, Jade excels at optimizing infrastructure and automating workflows to enhance efficiency and reliability.",
+    image:
+      "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80",
+  },
+];
 
 export default Team;
