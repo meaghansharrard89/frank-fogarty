ba8c70380d4a019b76bb2da8ce43e4b1dc64f189
Meaghan <meaghan.sharrard89@gmail.com>
2025-02-23 17:37:07 -0800

    Moved alumni and updated team photo

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -9,7 +9,7 @@ function Team() {
 
         <div class="mt-6 mb-14 px-4 lg:px-0 items-center">
           <img
-            src="https://i.ibb.co/CWXKW9q/editedlabimage.jpg"
+            src="https://i.ibb.co/Fq79NDg8/IMG-1555.jpg"
             alt="Team"
             class="mx-auto rounded-md"
           />
@@ -112,12 +112,6 @@ const teamMembers = [
     image: "https://i.ibb.co/2SPHqsT/frankedit.jpg",
   },
   {
-    name: "Brittany Welch",
-    job: "M.S. student",
-    bio: "I'm studying Lewis's woodpeckers in the Eastern Cascades of Oregon to understand how their abundance is influenced by habitat associations and landscape connectivity in burned and unburned forests.",
-    image: "https://i.ibb.co/K7cs4XS/brittanyedit.jpg",
-  },
-  {
     name: "Alon Averbuj",
     job: "M.S. student",
     bio: "Hi, I'm Alon. I grew up amongst the chaparral and beaches near Solana Beach, San Diego where I fell in love with ecology and GIS! I am studying the habitat suitability of the Humboldt Coast for common ravens, and their interactions with the threatened western snowy plovers.",
@@ -128,18 +122,6 @@ const teamMembers = [
     job: "M.S. student",
     bio: "Hiya! My name is Cameron! I grew up in Denver, Colorado, and completed my bachelor's in Conservation Biology from SUNY College of Environmental Science & Forestry. Since graduating, I have been lucky to work as a field scientist and environmental educator in some of the most beautiful places around the world like Mongolia, Yellowstone, the Northern Mariana Islands, the Sierra Nevada, and the White Mountains. I have worked with many different taxa, but birds stole my heart after my first field job studying red-headed woodpeckers. I am very excited to return to the wild world of woodpeckers with Drs. Frank Fogarty and Ho Yi Wan studying Lewis’s Woodpecker migration and behavioral ecology in central Oregon.",
     image: "https://i.ibb.co/BnLs2WK/cameronedit.jpg",
-  },
-  {
-    name: "Rachel Higuera",
-    job: "Honors undergraduate student",
-    bio: "I am a passionate birder investigating the influence of macroinvertebrate communities on waterfowl richness in the Arcata Marsh and Wildlife Sanctuary. I am very interested in avian behavior, migration, and trophic cascades.",
-    image: "https://i.ibb.co/0y3cG9T/racheledit.jpg",
-  },
-  {
-    name: "Kiana Elliott",
-    job: "Honors undergraduate student",
-    bio: "I'm an undergrad student majoring in wildlife management and conservation and minoring in environmental ethics. Currently, I'm preparing to work on my senior project, then I'll continue my education in grad school. I fell in love with birds when I took an Ornithology course and went birding for the first time. Working my first field technician job in Oregon solidified that I want to further study our feathered friends.",
-    image: "https://i.ibb.co/ftPLDPD/kianaedit.jpg",
   },
 ];
 
@@ -155,6 +137,24 @@ const alumniTeamMembers = [
     job: "Honors undergraduate student",
     bio: "I was able to work on my REU/Senior Thesis project with the Avian Ecology lab. My project looked at understanding microclimate factors influencing changes in bird biomass and species richness during the breeding season in the HJ Andrews Experimental forest. The degree to which microclimate influences bird biomass and species richness remains largely unexplored which piqued my interest in this project. I was able to gain, feedback, advice, and valuable insights via Frank and grad students on my project. I was also able to learn more about what it takes to do a scientific project by listening to lab discussions about models, presentations, and articles.",
     image: "https://i.ibb.co/PjX1n6q/jessicaedit.png",
+  },
+  {
+    name: "Kiana Elliott",
+    job: "Honors undergraduate student",
+    bio: "I'm an undergrad student majoring in wildlife management and conservation and minoring in environmental ethics. Currently, I'm preparing to work on my senior project, then I'll continue my education in grad school. I fell in love with birds when I took an Ornithology course and went birding for the first time. Working my first field technician job in Oregon solidified that I want to further study our feathered friends.",
+    image: "https://i.ibb.co/ftPLDPD/kianaedit.jpg",
+  },
+  {
+    name: "Rachel Higuera",
+    job: "Honors undergraduate student",
+    bio: "I am a passionate birder investigating the influence of macroinvertebrate communities on waterfowl richness in the Arcata Marsh and Wildlife Sanctuary. I am very interested in avian behavior, migration, and trophic cascades.",
+    image: "https://i.ibb.co/0y3cG9T/racheledit.jpg",
+  },
+  {
+    name: "Brittany Welch",
+    job: "M.S. student",
+    bio: "I'm studying Lewis's woodpeckers in the Eastern Cascades of Oregon to understand how their abundance is influenced by habitat associations and landscape connectivity in burned and unburned forests.",
+    image: "https://i.ibb.co/K7cs4XS/brittanyedit.jpg",
   },
 ];
 
